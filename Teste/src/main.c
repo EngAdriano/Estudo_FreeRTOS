e16ddb07727d443607d52255d88ab020c547c6fc
José Adriano <jose.filho@dellead.com>
2022-10-13 16:16:59 -0300

    Teste
    Teste de criação de arquivo.

---
+++ Teste/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_log.h"      
+
+void app_main()
+{
+    
+}
